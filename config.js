1bfd1d4f91d0ad8b7e1daf8e599748314fe26709
minhkkk1 <86712648+minhkkk1@users.noreply.github.com>
2021-06-30 15:47:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
-const CONFIG = {
-    introTitle: 'aaa',
-    introDesc: `Trái đất vốn lạ thường
+thconst CONFIG = {
+    introTitle: 'đọc dòng này là ngu',
+    introDesc: `bố cái thằng trẻ trâu namhai0303
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'sao mày ngu thế hả Hải',
+    desc: 'sao m chơi gà thế ',
+    btnYes: 'vì tao ngu',
+    btnNo: 'thì ai chả có trận này trận khác',
+    question:'nếu muốn làm thằng gà thì hãy tự thanh minh',
+    btnReply: 'Gửi cmm',
+    reply: 'ok tao biết tao ngu',
+    mess: 'ok tao cx biết là m ngu mà',
+    messDesc: 'chịu đây.',
+    btnAccept: 'thôi chịu',              
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    
 }
